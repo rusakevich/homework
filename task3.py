ae331a5b5984d7318755ab52953ed8a4b72405e8
rusakevich <rusakevich.v@gmail.com>
2017-05-21 18:32:21 +0300

    Задача3

--- task3.py
+++ task3.py
@@ -8,8 +8,9 @@ y2 = float(input('Y2 = '))
 x3 = float(input('X3 = '))
 y3 = float(input('Y3 = '))
 
-AB = ((x1-x2)**2+(y1-y2)**2)**0.5
-AC = ((x1-x3)**2+(y1-y3)**2)**0.5
-BC = ((x3-x2)**2+(y3-y2)**2)**0.5
+#Length in square
+AB = (x1-x2)**2+(y1-y2)**2
+AC = (x1-x3)**2+(y1-y3)**2
+BC = (x3-x2)**2+(y3-y2)**2
 
-print('Triangle is rectangular') if AB**2 == AC**2 + BC**2 or AC**2 == AB**2 + BC**2 or BC**2 == AB**2 + AC**2 else print('Triangle is not rectangular')
+print('Triangle is rectangular') if AB == AC + BC or AC == AB + BC or BC == AB + AC else print('Triangle is not rectangular')
