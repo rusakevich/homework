dcb52aa25a8a7afaa86e6b758e144be0f67afc9e
rusakevich <rusakevich.v@gmail.com>
2017-05-28 20:46:27 +0300

    ДЗ на 29.05

---
+++ Ежедневник/handler.py
@@ -0,0 +1,21 @@
+from menu_calendar import *
+def menu_handler():
+    paragraph = 1
+    while int(paragraph) != max(menu_all):
+        paragraph = input('Введите число: ')
+        if paragraph.isdigit() and int(paragraph) != 0:
+            print('{}\n'.format(menu_all.get(int(paragraph))())) if int(paragraph) in menu_all.keys() else print('Такого пункта меню ещё нет!!!\n')
+        else:
+            print('Ошибка!!! Введены некорректные данные!!!\n')
+            paragraph = 1
+
+
+print('Ежедневник. Выберите действие:\n')
+print('1. Вывести список задач')
+print('2. Добавить задачу')
+print('3. Отредактировать задачу')
+print('4. Завершить задачу')
+print('5. Начать задачу сначала')
+print('6. Выход\n')
+
+menu_handler()
---
+++ Ежедневник/menu_calendar.py
@@ -0,0 +1,14 @@
+def menu_1():
+    return 'Список задач'
+def menu_2():
+    return 'Добавьте задачу'
+def menu_3():
+    return 'Отредактируйте задачу'
+def menu_4():
+    return 'Завершите задачу'
+def menu_5():
+    return 'Начните задачу сначала'
+def menu_6():
+    return 'Выход'
+
+menu_all = {1:menu_1, 2:menu_2, 3:menu_3, 4:menu_4, 5:menu_5, 6:menu_6}
